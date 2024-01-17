d2f0c0460bb0fb4c203e6ab909bf04fd6cc01106
peramhe <151518540+peramhe@users.noreply.github.com>
2024-01-17 09:54:43 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,22 @@
+def is_prime(num):
+    """Check if a number is prime."""
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+def generate_primes(count):
+    """Generate the first 'count' prime numbers."""
+    primes = []
+    num = 2
+    while len(primes) < count:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+# Generate the first 100 prime numbers
+first_100_primes = generate_primes(100)
+print(first_100_primes)
